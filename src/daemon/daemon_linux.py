820bcdd4d660eae2dce4379608a66400262a55fa
enzzodani <gomes.enzodani@gmail.com>
2024-11-16 03:16:45 -0300

    daemon_linux initial

--- src/daemon/daemon_linux.py
+++ src/daemon/daemon_linux.py
@@ -1,9 +1,24 @@
 import socket
 import os
 
-
-
-
 def run_daemon():
     print("Run in linux")
 
+# Criação do Socket
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# Path do arquivo socket
+s_path = "/tmp/arq_socket"
+
+# Verificação se arq_socket ja existe
+if os.path.exists(s_path):
+    os.remove(s_path)
+
+# Vinculação
+s.bind((s_path))
+
+# Numero de conexoes
+s.listen(1)
+
+# Aceita a conexão
+conn, adrr = s.accept()
