a9a5fad45fd465a87b39cd0598e924dee6cc1cc1
enzzodani <gomes.enzodani@gmail.com>
2024-11-17 18:44:36 -0300

    server.c att

--- src/server/server.c
+++ src/server/server.c
@@ -2,10 +2,10 @@
 
 int start_server() {
     //Criar o socket
-    server_socket = socket(AF_UNIX, SOCK_STEAM, 0) //Arg 1: Familia do Socket, tipo de socket tcp ou udp e 0 para protocolo padrao
+    server_socket = socket(AF_UNIX, SOCK_STREAM, 0) //Arg 1: Familia do Socket, tipo de socket tcp ou udp e 0 para protocolo padrao
 
     if (server_socket < 0) { //Verificação de erro 
-        puts("Erro na criacao do server_socket.")
+        perror("Erro na criacao do server_socket.");
         return -1;
     }
 
@@ -13,17 +13,18 @@ int start_server() {
     struct sockaddr_un server_address; //declaração da estrutura de um endereço de socket
     memset(&server_address, 0, sizeof(server_address)); //Inicialização de todos os campos da estrutura como 0
     server_address.sun_family = AF_UNIX; //Familia do endereço
-    strcpy(server_address.sun_path, "/tmp/arq_socket"); //Caminho do endereço
+    const char* socket_path = "/tmp/arq_socket";
+    strcpy(server_address.sun_path, socket_path); //Caminho do endereço
  
-    if(bind(server_socket, (struct sockaddrr *)&server_address, sizeof(server_address) < 0)) { //Vinculção e verificação se houve erro ao vincular
-        puts("Erro ao vincular o server_socket");
+    if(bind(server_socket, (struct sockaddrr *)&server_address, sizeof(server_address)) < 0) { //Vinculção e verificação se houve erro ao vincular
+        perror("Erro ao vincular o server_socket");
         return -1;
         // Arg: Socket para vincular, endereço em forma de ponteiro para estrutura de socket de endereço e o tamanho do endereço
     }
 
     //Escuta
     if (listen(server_socket, 5) < 0) {
-        puts("Erro ao colocar o server_socket em listen");
+        perror("Erro ao colocar o server_socket em listen");
         return -1;
     }
 
@@ -39,7 +40,7 @@ void init_server() {
         //Aceitar
         int client_socket = accept(server_socket, NULL, NULL);
         if (client_socket < 0) {
-            puts("Erro ao aceitar a conexao");
+            perror("Erro ao aceitar a conexao");
             continue; //Tenta novamente
         }
 
@@ -62,18 +63,18 @@ void handle_client (int client_socket) {
     ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
     //Socket, buffer onde vai armazenar os dados, tamanho do buffer, e flag 0
     if (bytes_received < 0) {
-        puts("Erro ao receber dados do cliente");
+        perror("Erro ao receber dados do cliente");
         return;
     }
 
-    buffer[bytes_received] = '\0' //Garantir que o buffer seja uma string valida
+    buffer[bytes_received] = '\0'; //Garantir que o buffer seja uma string valida
     printf("Cliente: %s\n", buffer);
 
     //Resposta
     const char* response = "ACK Server";
     ssize_t bytes_sent = send(client_socket, response, strlen(response), 0);
     if (bytes_sent < 0) {
-        puts("Erro ao enviar dados para o cliente");
+        perror("Erro ao enviar dados para o cliente");
         return;
     }
 
